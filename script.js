6cd67d33c94260ec6f749d864d3b79939259b7f2
Rosana Manoel Venâncio <91646393+rosanavenancio@users.noreply.github.com>
2026-09-02 16:10:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function botao1() {
+    alert("Você clicou no primeiro botão");
+}
+
+function botao2() {
+    alert("Você clicou no segundo botão");
+}
+
+function botao3() {
+    alert("Você clicou no terceiro botão");
+}
